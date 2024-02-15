7ce0b5e30bfca08f1717bc13e5f7456a1b263089
Sumit8171 <126135239+Sumit8171@users.noreply.github.com>
2024-02-15 16:07:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-// Middleware to log request details
-app.use((req, res, next) => {
-    console.log(`Received ${req.method} request for ${req.url}`);
-    next(); // Move to the next middleware or route handler
-});
+app.get('/', (req, res) => {
+  res.send('Hello from the homepage!')
+})
 
-app.all('/', (req, res) => {
-    console.log("Just got a request!");
-    res.send('Yo!');
-});
+app.get('/about', (req, res) => {
+  res.send('This is the about page.')
+})
 
-// Custom route
-app.get('/hello', (req, res) => {
-    res.send('Hello from Express!');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(process.env.PORT || 3000)
